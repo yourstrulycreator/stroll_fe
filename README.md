9181d4cc11e38fcdb84b74cc00a162049956e3dd
Michael Nwankwo <mike.nwankwo@outlook.com>
2025-01-17 13:01:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# fe_interview_task
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a front end interview task for Flutter.
+
---
+++ android/app/src/main/kotlin/com/example/fe_interview_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fe_interview_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
